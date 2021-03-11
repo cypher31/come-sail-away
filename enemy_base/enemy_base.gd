4f5d292204b4beca0b6a24e69dba12c28bd46f65
Kelby Styler <kelbystyler@gmail.com>
2021-03-11 07:04:22 -0800

    Setup character and enemy scenes
    * User turn time tracked
    * Enemy and player attack collision handling added
    * Enemy logic for health loss and destruction added

---
+++ enemy_base/enemy_base.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+#enemy stats
+var health = 10
+var armor = 0
+
+onready var anim_player = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#
+#	pass
+
+func _enemy_hit(attack_strength):
+	if health > 0:
+		var damage_calc = attack_strength - armor
+		var damage_clamped = clamp(damage_calc, 1, damage_calc)
+		
+		health -= damage_clamped
+		anim_player.play("hit")
+		
+		print("DAMAGE: " + str(damage_clamped))
+		print("HEALTH LEFT: " + str(health))
+	else:
+		queue_free()
+	return
--- main_char_prototype/main_char_prototype.gd
+++ main_char_prototype/main_char_prototype.gd
@@ -5,56 +5,97 @@ var moving : bool = false #check if player is moving, can't attack while moving
 var direction : Vector2 = Vector2(0,0)
 var timer_attack #variable to hold the timers created to check for attack timing
 
+#character stats
+var health : int = 25
+var defense : int = 2
+var strength : int = 2 #character strength - used for damage calc
+var speed : int = 2 #character speed - used for turn time calc
+var turn_timer : Timer 
+
 onready var anim_player = $AnimationPlayer
 
-const SPEED = 250
+const SPEED = 5
 
 # need a signal to tell the character when it is their turn
 func _ready():
 	anim_player.set_current_animation("idle")
 	anim_player.connect("animation_finished", self, "_anim_finished")
+	
+	#turn time calc
+	var turn_time : float = 4.0 + speed / 10.0
+	
+	turn_timer = Timer.new()
+	turn_timer.set_wait_time(turn_time)
+	turn_timer.set_one_shot(true)
+	add_child(turn_timer)
+	turn_timer.start()
+	
+	#connections
+	$area2d_att_1.connect("body_entered", self, "_attack_collision")
+	$area2d_att_2.connect("body_entered", self, "_attack_collision")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	# Movement handling
 	if Input.is_action_pressed("north"):
 		direction.x = 0
 		direction.y = -1
 		moving = true
+		turn_timer.set_paused(false)
 	elif Input.is_action_pressed("south"):
 		direction.x = 0
 		direction.y = 1
 		moving = true
+		turn_timer.set_paused(false)
 	elif Input.is_action_pressed("east"):
 		direction.x = 1
 		direction.y = 0
 		moving = true
+		turn_timer.set_paused(false)
 	elif Input.is_action_pressed("west"):
 		direction.x = -1
 		direction.y = 0
 		moving = true
+		turn_timer.set_paused(false)
 	else:
 		direction.x = 0
 		direction.y = 0
 		moving = false
+		turn_timer.set_paused(true)
 		
 	var velocity : Vector2 = direction * SPEED
 	
-	move_and_slide(velocity)
+	var collision : KinematicCollision2D = move_and_collide(velocity)
 	
 	#attack animations
 	if !moving:
 		if Input.is_action_just_pressed("attack_basic") and anim_player.get_current_animation() == "idle":
 			anim_player.play("att_1")
+			turn_timer.set_paused(false)
 		elif Input.is_action_just_pressed("attack_basic") and anim_player.get_current_animation() == "att_1":
 			anim_player.play("att_2")
+			turn_timer.set_paused(false)
 		elif !anim_player.is_playing():
 			anim_player.play("idle")
+			turn_timer.set_paused(true)
+			
+	
+	#turn handling
+	if !turn_timer.paused:
+		print(turn_timer.get_time_left())
+	
 	pass
 	
 func _anim_finished(animation):
 	if !anim_player.is_playing():
 		anim_player.play("idle")
+	return
+
+func _attack_collision(body):
+	if body.is_in_group("enemy"):
+		body._enemy_hit(strength)
+		print("HIT!")
+		pass
 	return
